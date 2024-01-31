5cfd831dba1a1003e21c1cb96f38ccdf7632017e
Grigori Fursin <Grigori.Fursin@cTuning.org>
2024-01-31 11:49:43 +0100

    clean up

--- script/compile-website-ae/create_website.bat
+++ script/compile-website-ae/create_website.bat
@@ -1,5 +1,10 @@
 rem call local_env.bat
 
+set CK_CROWDSOURCE_PATH=D:\Work1\CK\ck-crowdsourcing
+set CK_REPOS=D:\Work1\CK\ck-repos
+set CK_ROOT=D:\Work1\CK\ck
+set CK_TOOLS=D:\Work1\CK\ck-tools
+
 set CK_WFE_URL_PREFIX_SKIP=yes
 rem set CK_WFE_URL_PREFIX=
 
